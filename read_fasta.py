08e6788929c8be5463617aac6655be47949a61c9
wodanaz <alebesc@gmail.com>
2016-10-18 10:06:12 -0400

    Prints Usage if no file provided.
    It became annoying that the program would just crash if I forgot to provide an  argument.
    Fortunatelly I no typos here

--- read_fasta.py
+++ read_fasta.py
@@ -11,4 +11,8 @@ def read_fasta(filename):
     f.close()
     return sequence
 
+if len(sys.argv) < 2:
+    print "Usage:", sys.argv[0], "<sequence.fa>"
+    exit(1)
+
 print read_fasta(sys.argv[1])
