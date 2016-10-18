a540853bd3e5434b64027a084041c9e40f590e2e
wodanaz <alebesc@gmail.com>
2016-10-18 09:59:16 -0400

    Replaces hard-coded file with command line parametter.

--- read_fasta.py
+++ read_fasta.py
@@ -1,3 +1,5 @@
+import sys
+
 def read_fasta(filename):
     sequence = ''
     f = open(filename)
@@ -9,4 +11,4 @@ def read_fasta(filename):
     f.close()
     return sequence
 
-print read_fasta('ae.fa')
+print read_fasta(sys.argv[1])
